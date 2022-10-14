ccf0392797197346982b85e90196327bf4c5f700
asloth <sbenelramirez@gmail.com>
2022-10-14 02:16:59 -0500

    feat(server): add routes and connect db

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"github.com/asloth/market-backend/database"
+	"github.com/asloth/market-backend/src/product"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
+	dsn := "bsale_test:bsale_test@tcp(mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com)/bsale_test?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	database.Handler = database.NewBaseHandler(db)
+
+	app := fiber.New()
+
+	app.Use(cors.New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Get("/products/:category?", product.HandlerProducts)
+
+	app.Listen(":3000")
+}
